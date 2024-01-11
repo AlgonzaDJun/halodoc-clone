b88e7106468be29c8cc8671260d15e8ed6de92f1
AlgonzaDJun <algonsadewangga@gmail.com>
2024-01-11 22:55:54 +0900

    finish article

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'abu' : '#F5F8FC',
-				'text-abu' : '#333333'
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#DF024D",
+        abu: "#F5F8FC",
+        "text-abu": "#333333",
+		'btn-bg': '#FBE6ED',
+      },
+    },
+  },
+  plugins: [],
+};
